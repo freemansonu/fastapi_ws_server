f563537161b3e35e27d282560375fcd154075727
sonu <freemansonu@gmail.com>
2025-10-28 17:49:19 -0500

    Cleaned the code

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     print("🔌 Client connected!")
     while True:
-        print(".")
         data = await websocket.receive_text()   # Receive message from Flutter
         print("📩 Received:", data)
         await websocket.send_text(f"Server says: Hi, you said '{data}'!")  # Send reply
