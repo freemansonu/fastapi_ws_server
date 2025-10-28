e6f5ad8d63724513cea9b754c4d3ec984fe0b362
sonu <freemansonu@gmail.com>
2025-10-28 14:25:22 -0500

    Removed the frequent messages from server

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, WebSocket
-import asyncio
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -7,24 +7,8 @@ app = FastAPI()
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     print("🔌 Client connected!")
-
-    async def send_waiting():
-        while True:
-            await asyncio.sleep(3)  # Wait 3 minute
-            try:
-                await websocket.send_text("#")
-            except Exception as e:
-                print("⚠️ Error sending waiting:", e)
-                break
-
-    # Start background task
-    asyncio.create_task(send_waiting())
-
-    # Main loop: receive messages from client
-    try:
-        while True:
-            data = await websocket.receive_text()
-            print("📩 Received:", data)
-            await websocket.send_text(f"Server says: Hi, you said '{data}'!")
-    except Exception as e:
-        print("❌ Client disconnected:", e)
+    while True:
+        print(".")
+        data = await websocket.receive_text()   # Receive message from Flutter
+        print("📩 Received:", data)
+        await websocket.send_text(f"Server says: Hi, you said '{data}'!")  # Send reply
